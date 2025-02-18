d6f2d0adb95ef8f2fb374ad8558588127176a7b6
Birendra Singh <30977841+vsbsc6@users.noreply.github.com>
2025-02-18 13:13:20 +0000

    Editing FileManager

--- app/src/main/java/net/chittu/codepad/FileManager.java
+++ app/src/main/java/net/chittu/codepad/FileManager.java
@@ -4,8 +4,71 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FileManager {
-    private HashMap<String, CodeFile> codeFiles = new HashMap<>();
-    private ArrayList<String> locations = new ArrayList<>();
+    public static final String AUTOSAVED_DIRECTORY_NAME = "autosaved";
+    private File autosavedFilesDirectory;
+    private HashMap<String, CodeFile> codeFiles;
+    private ArrayList<String> locations;
+    private int untitledCount;
+
+    public FileManager(Context context){
+        File root = context.getFilesDir();
+        File autosavedFilesDirectory = new File(root, AUTOSAVED_DIRECTORY_NAME);
+        if(!autosavedFilesDirectory.exists()){
+            autosavedFilesDirectory.mkdirs();
+        }
+
+
+
+        codeFiles = new HashMap<>();
+        locations = new ArrayList<>();
+        untitledCount = 0;
+    }
+
+    public int getCount(){
+        return codeFiles.size();
+    }
+
+    public CodeFile getFile(){
+        String location = locations.get(position);
+        return codeFiles.get(location);
+    }
+
+    public CodeFile addFile(){
+        String filename = "Untitled " + (++untitledCount);
+        Uri uri = Uri.parse("codepad://" + filename);
+
+    }
+
+    public int openFile(CodeFile codeFile){
+        String location = codeFile.getUri().toString();
+        if(locations.contains(location)){
+            return locations.indexOf(location);
+        }
+        else{
+            locations.add(location);
+            codeFiles.put(location, codeFile);
+            int index = locations.size() - 1;
+            return index;
+        }
+    }
+
+    public void closeFile(int position){
+        if(position < 0 || position >= getItemCount())
+            return;
+
+        String location = locations.remove(position);
+        codeFiles.remove(location);
+    }
+
+
+
+
+
+
+
+
+
+
 
     public int getItemCount() {
         return codeFiles.size();
